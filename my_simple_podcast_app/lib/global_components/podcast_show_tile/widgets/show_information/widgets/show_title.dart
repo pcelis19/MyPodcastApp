2df2a9cb4817c9fa654c922cb32391e627c0318a
pcelis2 <49768662+pcelis2@users.noreply.github.com>
2020-09-10 00:30:46 -0700

    Update show_title.dart

--- my_simple_podcast_app/lib/global_components/podcast_show_tile/widgets/show_information/widgets/show_title.dart
+++ my_simple_podcast_app/lib/global_components/podcast_show_tile/widgets/show_information/widgets/show_title.dart
@@ -1,6 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:my_simple_podcast_app/global_models/podcast_show.dart';
 
 class ShowTitle extends StatelessWidget {
   const ShowTitle({
